24eeec498539ed82aa1b5b9eda22fe2ae41f7091
Dan-393 <54181600+Dan-393@users.noreply.github.com>
2022-08-17 13:45:52 +0800

    完成分包配置,tabBar相关的四个页面方主包，其余放分包

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+
+$http.beforeRequest = function(options){
+	uni.showLoading({
+	    title: '数据加载中...',
+	  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  wx.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		
+		<!-- 搜索栏 -->
+		<!-- 轮播图 -->
+		<swiper circular :indicator-dots="indicatorDots" :autoplay="autoplay">
+			<swiper-item v-for="item in swiperList" :key="item.goods_id">
+				<image :src="item.image_src"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +14,40 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据
+				swiperList:[],
+				// 轮播图配置
+				indicatorDots:true,
+				autoplay: true,
 			};
-		}
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods:{
+			// 请求轮播图数据
+			async getSwiperList(){
+				const{data:res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if(res.meta.status !== 200){
+					return uni.showToast({
+						title:"请求数据失败",
+						icon:'none'
+					})
+				}
+				this.swiperList = res.message
+			}
+		},
+		
 	}
 </script>
 
 <style lang="scss">
-
+	swiper {
+		height: 330rpx;
+		swiper-item image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+	
 </style>
